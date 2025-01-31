749703b17a4e02fe4b17f2a6da56c55f4bc9275b
Anastasia Kulesh <kulesh.anastasia.s@gmail.com>
2025-01-31 07:11:00 +0300

    feat: add routing for logo and save location in protected route

--- src/components/protected-route/ProtectedRoute.tsx
+++ src/components/protected-route/ProtectedRoute.tsx
@@ -1,7 +1,7 @@
 import { getUserState } from '../../services/slices/userSlice';
 import { useSelector } from '../..//services/store';
 import { Preloader } from '../ui/preloader/preloader';
-import { Navigate } from 'react-router-dom';
+import { Navigate, useLocation } from 'react-router-dom';
 
 type ProtectedRouteProps = {
   onlyUnAuth?: boolean;
@@ -13,6 +13,7 @@ export const ProtectedRoute = ({
   children
 }: ProtectedRouteProps) => {
   const { authCheck, user } = useSelector(getUserState);
+  const location = useLocation();
 
   if (!authCheck) {
     // пока идёт чекаут пользователя, показываем прелоадер
@@ -21,7 +22,8 @@ export const ProtectedRoute = ({
 
   if (onlyUnAuth && !user) {
     // если пользователь на странице авторизации и данных в хранилище нет, то делаем редирект
-    return <Navigate replace to='/login' />;
+    const from = location.state?.from || { pathname: '/' };
+    return <Navigate replace to={from} />;
   }
 
   return children;
