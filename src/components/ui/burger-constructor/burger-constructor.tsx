c6cfdc15716455e2ce5d9ae99c078d0afde7e021
Anastasia Kulesh <kulesh.anastasia.s@gmail.com>
2025-02-02 12:29:43 +0300

    test: add create order

--- src/components/ui/burger-constructor/burger-constructor.tsx
+++ src/components/ui/burger-constructor/burger-constructor.tsx
@@ -79,6 +79,7 @@ export const BurgerConstructorUI: FC<BurgerConstructorUIProps> = ({
         <CurrencyIcon type='primary' />
       </div>
       <Button
+        data-cy='order'
         htmlType='button'
         type='primary'
         size='large'
